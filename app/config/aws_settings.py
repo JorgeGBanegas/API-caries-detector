ff25d2ce0ca636ae5a7bbb3501b1c997fe5fa9ff
Jorge Banegas <jorge.g.banegas@gmail.com>
2023-05-30 15:27:09 -0400

    reactor: cambiar la funcionalidad de prediccion local a prediccion en la nube

--- app/config/aws_settings.py
+++ app/config/aws_settings.py
@@ -10,6 +10,7 @@ class AwsSetting(BaseSettings):
     check_expiration: bool = True
     jwt_header_prefix: str = "Bearer"
     jwt_header_name: str = "Authorization"
+    endpoint_sagemaker: str = os.getenv("ENDPOINT_MODEL_SAGEMAKER")
     aws_credentials: dict = {
         "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
         "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
--- app/services/analysis_service.py
+++ app/services/analysis_service.py
@@ -1,10 +1,10 @@
+import base64
 import io
+import json
 import os
-import keras as keras
 import numpy as np
 from PIL import Image
 import boto3
-
 
 from app.config.aws_settings import AwsSetting
 
@@ -13,49 +13,40 @@ aws_credentials = aws_setting.aws_credentials
 aws_access_key_id = aws_credentials["aws_access_key_id"]
 aws_secret_access_key = aws_credentials["aws_secret_access_key"]
 aws_region = aws_setting.userpools["sa"]["region"]
+endpoint_name = aws_setting.endpoint_sagemaker
 
 
 def _load_image(image_file):
     image_stream = io.BytesIO(image_file)
     image = Image.open(image_stream)
     image_array = np.array(image)
+    image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
 
-def _load_model():
-    current_dir = os.path.dirname(__file__)
-    parent_dir = os.path.dirname(current_dir)
-    model_path = os.path.join(parent_dir, 'model_ai', 'best_model.h5')
-    model_directory = os.path.dirname(model_path)
-    if os.path.exists(model_path):
-        model = keras.models.load_model(model_path)
-        return model
+def _predict(image):
+    runtime_client = boto3.client('runtime.sagemaker', region_name=aws_region)
+    payload = {
+        'instances': image.tolist()
+    }
+    payload_json = json.dumps(payload)
+    response = runtime_client.invoke_endpoint(EndpointName=endpoint_name,
+                                              ContentType='application/json',
+                                              Body=payload_json)
 
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=aws_region
-    )
-    bucket_name = 'intellident-model'
-    object_name = 'best_model.h5'
-    file_name = os.path.join(model_directory, 'best_model.h5')
-    s3.download_file(bucket_name, object_name, file_name)
-
-    os.rename(file_name, model_path)
-
-    model = keras.models.load_model(model_path)
-
-    return model
+    result = json.loads(response['Body'].read().decode())
+    return result
 
 
 class AnalysisService:
     @staticmethod
     def analyze_image(image_file):
-        model = _load_model()
         image = _load_image(image_file)
-        prediction = model.predict(np.expand_dims(image, axis=0), verbose=0)[0]
-        probability = prediction[0]
+        prediction = _predict(image)
+        print(prediction)
+        probability = prediction['predictions'][0][0]
         label = "Caries" if probability > 0.5 else "No Caries"
         percentage_probability = probability * 100
+        # limit to 2 decimal places
+        percentage_probability = round(percentage_probability, 2)
         return label, percentage_probability
